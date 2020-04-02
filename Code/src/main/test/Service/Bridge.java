722db781bf7cf77cb0883dc69fe0ad12f978776b
Amir Loewenthal <amirloe@post.bgu.ac.il>
2020-04-02 10:43:36 +0300

    Bridge interface

---
+++ Code/src/main/test/Service/Bridge.java
@@ -0,0 +1,6 @@
+package Service;
+
+public interface Bridge {
+
+    public boolean login (String username , String password);
+}
---
+++ Code/src/main/test/Service/ProxyBridge.java
@@ -0,0 +1,15 @@
+package Service;
+
+public class ProxyBridge implements Bridge {
+    private RealBridge rb = null;
+
+
+    public boolean login(String username, String password) {
+        if (rb != null) {
+            return rb.login(username, password);
+        }
+        else {
+            return false;
+        }
+    }
+}
