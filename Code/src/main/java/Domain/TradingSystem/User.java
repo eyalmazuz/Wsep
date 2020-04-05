789e21dc98d67ef397a9e330235219969b74fbc5
Amir Loewenthal <amirloe@post.bgu.ac.il>
2020-04-05 17:27:49 +0300

    UseCase 4.1

--- Code/src/main/java/Domain/TradingSystem/User.java
+++ Code/src/main/java/Domain/TradingSystem/User.java
@@ -1,4 +1,17 @@
 package Domain.TradingSystem;
 
 public class User {
+
+    private Permission permissions;
+    private UserState state;
+
+    public boolean addProductToStore(int storeId, int productId, int ammount) {
+        Store currStore = permissions.hasPermission(storeId,"Owner");
+        if(currStore != null){
+            return state.addProductToStore(currStore,productId,ammount);
+        }
+        return false;
+
+    }
+
 }
