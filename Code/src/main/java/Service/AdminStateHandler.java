9186d88c94a60f30308d44c12904548ceb5eb364
Amir Loewenthal <amirloe@post.bgu.ac.il>
2020-04-22 16:54:45 +0300

    Service layer for usecase 6.*

---
+++ Code/src/main/java/Service/AdminStateHandler.java
@@ -0,0 +1,31 @@
+package Service;
+
+import Domain.TradingSystem.System;
+
+public class AdminStateHandler {
+
+    private int sessionId;
+    private System s = System.getInstance();
+
+    public AdminStateHandler(int sessionId){
+        this.sessionId = sessionId;
+    }
+
+    //uscase 6.4.1
+    public String getSubscriberHistory(int subId){
+        if(s.isAdmin(sessionId)){
+            return s.getUserHistory(subId);
+        }
+        return null;
+    }
+
+    //uscase 6.4.2
+    public String getStoreHistory(int storeId){
+        if(s.isAdmin(sessionId)){
+            return s.getStoreHistoryAsAdmin(storeId);
+        }
+        return null;
+
+    }
+
+}
