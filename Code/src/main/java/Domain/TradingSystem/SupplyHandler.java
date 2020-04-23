a76af9f6cdf8d320f1d9185c79392dcab86d46cc
Amir Loewenthal <amirloe@post.bgu.ac.il>
2020-04-23 16:37:46 +0300

    minor bug fixes (null assumptions etc..)

--- Code/src/main/java/Domain/TradingSystem/SupplyHandler.java
+++ Code/src/main/java/Domain/TradingSystem/SupplyHandler.java
@@ -5,7 +5,10 @@ import java.util.Map;
 public class SupplyHandler {
     private final String config;
 
-    public SupplyHandler(String config) {
+    public SupplyHandler(String config) throws Exception {
+        if (config.equals("Error")){
+            throw new Exception();
+        }
         this.config = config;
     }
 
