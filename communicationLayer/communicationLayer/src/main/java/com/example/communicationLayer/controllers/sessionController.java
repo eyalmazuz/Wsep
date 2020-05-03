eab6c3b54adf2f97fd3ddc387776ee03285bdda7
Rotem <rotemha@post.bgu.ac.il>
2020-05-03 18:23:06 +0300

    communication layer update

--- communicationLayer/communicationLayer/src/main/java/com/example/communicationLayer/controllers/sessionController.java
+++ communicationLayer/communicationLayer/src/main/java/com/example/communicationLayer/controllers/sessionController.java
@@ -1,22 +1,38 @@
 package com.example.communicationLayer.controllers;
 
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import Service.*;
 import com.example.communicationLayer.CommunicationLayerApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.Session;
 
 @RestController
 public class sessionController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    SessionHandler sessionHandler = new SessionHandler();
+
 
     @GetMapping("/setup")
-    public void setup(@RequestParam(value = "supplyConfig", defaultValue = "") String supplyConfig) {
+    @ResponseBody
+    public boolean setup(@RequestParam(value = "supplyConfig", defaultValue = "") String supplyConfig,
+                      @RequestParam(value = "paymentConfig", defaultValue = "") String paymentConfig) {
+        return sessionHandler.setup(supplyConfig,paymentConfig);
 
-        new CommunicationLayerApplication(counter.incrementAndGet(), String.format(template, name));
     }
+
+    @GetMapping("/startSession")
+    @ResponseBody
+    public int startSession(@RequestParam(value = "supplyConfig", defaultValue = "") String supplyConfig,
+                         @RequestParam(value = "paymentConfig", defaultValue = "") String paymentConfig) {
+        return sessionHandler.startSession();
+
+    }
+
+
+
+
 }
