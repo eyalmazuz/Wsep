a54c6a810ff29d1a6c3f2ddc2acbc46a8257a840
Omrimg2 <omrimg2@gmail.com>
2020-06-13 16:57:22 +0300

    base stats class

---
+++ communicationLayer/WSEP/src/main/java/Domain/TradingSystem/DayStatistics.java
@@ -0,0 +1,17 @@
+package Domain.TradingSystem;
+
+import java.util.Date;
+
+public class DayStatistics {
+
+    private Date date;
+
+    private int guests;
+    private int regularSubs;
+    private int managersNotOwners;
+    private int managersOwners;
+    private int admins;
+
+    
+
+}
